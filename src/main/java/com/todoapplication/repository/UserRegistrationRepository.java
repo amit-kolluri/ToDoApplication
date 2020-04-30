7155c7b99dd4ec0ce2d04b4886103c2a61a81a8b
Amit <ak.6170@outlook.com>
2020-04-29 19:53:24 -0500

    added version 0.5

--- src/main/java/com/todoapplication/repository/UserRegistrationRepository.java
+++ src/main/java/com/todoapplication/repository/UserRegistrationRepository.java
@@ -2,7 +2,9 @@ package com.todoapplication.repository;
 
 import com.todoapplication.model.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRegistrationRepository extends JpaRepository<UserDetails, Long> {
 
     //  public String saveAll(String user_name, String email, String phone, String address, String city, String zip, String state, String country);
